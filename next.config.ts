7bd3e7457b008f18fb4e7984deb5ba0ab8b4719c
okanacer332 <acer.okanumut@hotmail.com>
2025-07-04 19:52:39 +0300

    Temizlik Blog

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Bu satırı ekle veya var olanı bu şekilde değiştir
+  images: {
+    unoptimized: true, // GitHub Pages'te Next.js Image optimizasyonu çalışmaz, bu yüzden devre dışı bırakıyoruz.
+  },
+  // Diğer mevcut ayarlarınız buraya gelecek
 };
 
-export default nextConfig;
+module.exports = nextConfig;
